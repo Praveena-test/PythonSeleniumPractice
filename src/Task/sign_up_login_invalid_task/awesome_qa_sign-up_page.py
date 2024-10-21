055e98d518f849d1ad02811b56ade060b88c748b
Praveena <pravi21594@gmail.com>
2024-10-21 22:01:14 +0530

    Tasks of Oct-19-2024 updated

--- src/Task/sign_up_login_invalid_task/awesome_qa_sign-up_page.py
+++ src/Task/sign_up_login_invalid_task/awesome_qa_sign-up_page.py
@@ -8,7 +8,7 @@ import time
 def test_sign_up():
     first_name="Ratan"
     last_name="Tata"
-    email="rtata4@gmail.com"
+    email="rtata6@gmail.com"
     telephone="1234567890"
     password="Qwerty@123"
     confirm_password="Qwerty@123"
@@ -16,23 +16,23 @@ def test_sign_up():
     driver = webdriver.Chrome()
     driver.get("https://awesomeqa.com/ui/index.php?route=account/register")
     time.sleep(5)
-    first_name_element= driver.find_element(By.XPATH, "//*[@id='input-firstname']")
-    last_name_element=driver.find_element(By.XPATH,"//*[@id='input-lastname']" )
-    email_element=driver.find_element(By.XPATH,"//*[@id='input-email']" )
-    telephone_element=driver.find_element(By.XPATH, "//*[@id='input-telephone']")
-    password_element=driver.find_element(By.XPATH, "//*[@id='input-password']" )
-    confirm_password_element=driver.find_element(By.XPATH, "//*[@id='input-confirm']" )
+    first_name_element= driver.find_element(By.XPATH, "//input[@id='input-firstname']")
+    last_name_element=driver.find_element(By.XPATH,"//input[@id='input-lastname']" )
+    email_element=driver.find_element(By.XPATH,"//input[@id='input-email']" )
+    telephone_element=driver.find_element(By.XPATH, "//input[@id='input-telephone']")
+    password_element=driver.find_element(By.XPATH, "//input[@id='input-password']" )
+    confirm_password_element=driver.find_element(By.XPATH, "//input[@id='input-confirm']" )
     first_name_element.send_keys(first_name)
     last_name_element.send_keys(last_name)
     email_element.send_keys(email)
     telephone_element.send_keys(telephone)
     password_element.send_keys(password)
     confirm_password_element.send_keys(confirm_password)
-    checkbox_element=driver.find_element(By.XPATH, "//*[@id='content']/form/div/div/input[1]")
+    checkbox_element=driver.find_element(By.XPATH, "//input[@name='agree']")
     if not checkbox_element.is_selected():
             checkbox_element.click()
 
-    submit_element=driver.find_element(By.XPATH, "//*[@id='content']/form/div/div/input[2]")
+    submit_element=driver.find_element(By.XPATH, "//input[@value='Continue']")
     submit_element.click()
     current_url = driver.current_url
     print(current_url)
