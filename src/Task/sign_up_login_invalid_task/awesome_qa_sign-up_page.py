ab945fb5328fe637b93bca2457977f4d01a08b27
Praveena <pravi21594@gmail.com>
2024-10-21 21:41:45 +0530

    Tasks of Oct-19-2024 updated

---
+++ src/Task/sign_up_login_invalid_task/awesome_qa_sign-up_page.py
@@ -0,0 +1,45 @@
+# Open - https://awesomeqa.com/ui/index.php?route=account/register
+# Fill the form
+# Verify that next page give - Your Account Has Been Created!
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+def test_sign_up():
+    first_name="Ratan"
+    last_name="Tata"
+    email="rtata4@gmail.com"
+    telephone="1234567890"
+    password="Qwerty@123"
+    confirm_password="Qwerty@123"
+
+    driver = webdriver.Chrome()
+    driver.get("https://awesomeqa.com/ui/index.php?route=account/register")
+    time.sleep(5)
+    first_name_element= driver.find_element(By.XPATH, "//*[@id='input-firstname']")
+    last_name_element=driver.find_element(By.XPATH,"//*[@id='input-lastname']" )
+    email_element=driver.find_element(By.XPATH,"//*[@id='input-email']" )
+    telephone_element=driver.find_element(By.XPATH, "//*[@id='input-telephone']")
+    password_element=driver.find_element(By.XPATH, "//*[@id='input-password']" )
+    confirm_password_element=driver.find_element(By.XPATH, "//*[@id='input-confirm']" )
+    first_name_element.send_keys(first_name)
+    last_name_element.send_keys(last_name)
+    email_element.send_keys(email)
+    telephone_element.send_keys(telephone)
+    password_element.send_keys(password)
+    confirm_password_element.send_keys(confirm_password)
+    checkbox_element=driver.find_element(By.XPATH, "//*[@id='content']/form/div/div/input[1]")
+    if not checkbox_element.is_selected():
+            checkbox_element.click()
+
+    submit_element=driver.find_element(By.XPATH, "//*[@id='content']/form/div/div/input[2]")
+    submit_element.click()
+    current_url = driver.current_url
+    print(current_url)
+    time.sleep(2)
+
+    assert current_url=="https://awesomeqa.com/ui/index.php?route=account/success"
+    expected_headline="Your Account Has Been Created!"
+    observed_headline=driver.find_element(By.TAG_NAME,"h1")
+    print(observed_headline.text)
+    assert observed_headline.text == expected_headline
---
+++ src/Task/sign_up_login_invalid_task/vwo_login_invalid.py
@@ -0,0 +1,45 @@
+# [Assignment] - Invalid error message Capture for the Login Page of VWO.com
+# Fetch the locators - https://app.vwo.com/
+# Create a Python Project
+# Add the Allure Report (Allure Pytest)
+# Automate the two Test cases of VWO.com
+# Invalid Username and Valid Password.
+# Capture the error and pass the test.
+# Run them and share results
+import time
+from operator import contains
+from os import times
+
+import pytest
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_invalid_login_check():
+    driver = webdriver.Chrome()
+    driver.get("https://app.vwo.com/#/login")
+    assert driver.current_url == "https://app.vwo.com/#/login"
+    invalid_username = "abc@gmail.com"
+    invalid_password = "Qwersdfgt"
+
+    username_textbox = driver.find_element(By.XPATH, "//input[@id='login-username']")
+    username_textbox.clear()
+    username_textbox.send_keys(invalid_username)
+    password_textbox = driver.find_element(By.XPATH, "//input[@name='password']")
+    password_textbox.clear()
+    password_textbox.send_keys(invalid_password)
+    login_button = driver.find_element(By.ID, "js-login-btn")
+    login_button.click()
+
+    # <div
+    # class="notification-box-description"
+    # id="js-notification-box-msg"
+    # data-qa="rixawilomi">
+    # Your email, password, IP address or location did not match
+    # </div>
+    time.sleep(3)
+    expected_error_msg = "Your email, password, IP address or location did not match"
+    observed_error_msg = driver.find_element(By.ID, "js-notification-box-msg")
+    assert observed_error_msg.text == expected_error_msg
+    print(observed_error_msg.text)
